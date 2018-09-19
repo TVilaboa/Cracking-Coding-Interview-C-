d5e13f0988044eadee4e4dc3bd319c1447187cc7
Tomas Vilaboa <tvilaboa@hotmail.com>
2018-09-19 10:21:43 -0300

    Add project files.

---
+++ Craking The Coding Interview/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Craking_The_Coding_Interview
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
