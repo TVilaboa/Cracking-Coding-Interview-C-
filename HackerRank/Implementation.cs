83330d10c9b1294344df13e1de4ff10422f41130
Tomas Vilaboa <tvilaboa@hotmail.com>
2018-10-11 11:45:17 -0300

    First Implementation problems HackerRank

---
+++ HackerRank/Implementation.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HackerRank
+{
+    public class Implementation
+    {
+        public static int[] GradingStudents(int[] grades)
+        {
+            for (var index = 0; index < grades.Length; index++)
+            {
+                var grade = grades[index];
+                if (grade > 37)
+                {
+                    if ((grade + 1) % 5 == 0)
+                    {
+                        grades[index] = grade + 1;
+                    }
+                    else if ((grade + 2) % 5 == 0)
+                    {
+                        grades[index] = grade + 2;
+                    }
+                }
+            }
+
+            return grades;
+        }
+
+        public static int[] CountApplesAndOranges(int s, int t, int a, int b, int[] apples, int[] oranges)
+        {
+            var applesInRange = 0;
+            var orangesInRange = 0;
+            foreach (var apple in apples)
+            {
+                if(apple+a >= s && apple + a <= t)applesInRange += 1;
+            }
+            foreach (var orange in oranges)
+            {
+                if (orange + b >= s && orange + b <= t) orangesInRange += 1;
+            }
+
+            Console.WriteLine(applesInRange);
+            Console.WriteLine(orangesInRange);
+            return new[] {applesInRange, orangesInRange};
+        }
+
+        public static string Kangaroo(int x1, int v1, int x2, int v2)
+        {
+            //If x1+z*v1-x2-z*v2=0, yes, else, no
+            //z=(x2-x1)/(v1-v2)
+            if (v1 == v2 && x1 != x2)
+                return "NO";
+            var stepsNeeded = Decimal.Divide((x2-x1),(v1-v2));
+            if(stepsNeeded >= 0 && stepsNeeded % 1 == 0)
+            {
+                return "YES";
+            }
+            else
+            {
+                return "NO";
+            }
+
+        }
+
+       public static int GetTotalX(int[] a, int[] b)
+       {
+           var counter = 0;
+            foreach (var i in Enumerable.Range(a.Max(),b.Min()))
+            {
+
+               if(a.All(aNumber => i%aNumber == 0) && b.All(bNumber => bNumber%i == 0))
+                counter += 1;
+            }
+
+           return counter;
+       }
+
+        public static int[] BreakingRecords(int[] scores)
+        {
+            int max = scores[0];
+             int min = scores[0];
+            int counterMax = 0;
+            int counterMin = 0;
+            foreach (var score in scores)
+            {
+                if (score > max)
+                {
+                    max = score;
+                    counterMax += 1;
+                }else if (score < min)
+                {
+                    min = score;
+                    counterMin += 1;
+                }
+            }
+
+            return new[] {counterMax, counterMin};
+        }
+    }
+}
---
+++ HackerRankTests/ImplementationTest.cs
@@ -0,0 +1,106 @@
+﻿using HackerRank;
+using Xunit;
+
+namespace HackerRankTests
+{
+    public class ImplementationTest
+    {
+        //Sample Input 0
+
+        //4
+        //73
+        //67
+        //38
+        //33
+        //Sample Output 0
+
+        //75
+        //67
+        //40
+        //33
+        [Fact]
+        public void GradingStudents()
+        {
+            Assert.Equal(new int[] { 75, 67, 40, 33 }, Implementation.GradingStudents(new int[] { 73, 67, 38, 33 }));
+
+        }
+
+        //Sample Input 0
+
+        //7 11
+        //5 15
+        //3 2
+        //-2 2 1
+        //5 -6
+        //Sample Output 0
+
+        //1
+        //1
+        [Fact]
+        public void CountApplesAndOranges()
+        {
+            Assert.Equal(new int[]{1,1},Implementation.CountApplesAndOranges(7,11,5,15,new []{-2,2,1},new []{5,-6}));
+        }
+
+        //Sample Input 0
+
+        //0 3 4 2
+        //Sample Output 0
+
+        //YES
+     
+        //    Sample Input 1
+
+        //0 2 5 3
+        //Sample Output 1
+
+        //NO
+        //Sample Input 2
+        //21 6 47 3
+        [Theory]
+        [InlineData("NO",0, 2, 5, 3)]
+        [InlineData("NO",21, 6, 47, 3)]
+        [InlineData("YES", 0, 3, 4, 2)]
+     
+       public void Kangaroo(string expected,int x1, int v1, int x2, int v2)
+        {
+            Assert.Equal(expected, Implementation.Kangaroo(x1,v1,x2,v2));
+
+        }
+
+        //Sample Input
+
+        //2 3
+        //2 4
+        //16 32 96
+        //Sample Output
+
+        //3
+
+        [Theory]
+        [InlineData(3,new[]{2,4},new[]{16,32,96})]
+       
+
+        public void GetTotalX(int expected, int[] a,int[] b)
+        {
+            Assert.Equal(expected, Implementation.GetTotalX(a,b));
+
+        }
+
+        //Sample Input 1
+
+        //10
+        //3 4 21 36 10 28 35 5 24 42
+        //Sample Output 1
+
+        //4 0
+        [Theory]
+        [InlineData(new[] { 4,0}, new[] { 3,4,21,36,10,28,35,5,24,42 })]
+        public void BreakingRecords(int[] expected, int[] scores)
+        {
+            Assert.Equal(expected, Implementation.BreakingRecords(scores));
+
+        }
+
+    }
+}
